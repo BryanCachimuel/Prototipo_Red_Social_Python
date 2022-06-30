807ea165e041e9957bff2f85e84ddd1d555dddfb
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-29 20:28:52 -0500

    creando la función para realizar el registro de usuarios

--- social_django/redsocialdj/views.py
+++ social_django/redsocialdj/views.py
@@ -1,5 +1,7 @@
+from email import message
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
 """
 video 2. Creación de las vistas para verificar si los templates estan correctamente puestos.
@@ -14,6 +16,19 @@ def feed(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'social/feed.html', context)
+
+# video 5.1 -> invocación a la vista para el registro de usuarios
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            message.success(request, f'Usuario {username} creado satisfactoriamente')
+    else:
+        form = UserCreationForm()
+    
+    context = {'form':form}
+    return render(request, 'social/register.html', context)
 
 def profile(request):
     return render(request, 'social/profile.html')
