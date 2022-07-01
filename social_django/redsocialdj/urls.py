13b29c1bdc0afa30665495b0688991a2eb2e539e
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-30 20:44:21 -0500

    agregando la url para visitar el perfil de los usuarios

--- social_django/redsocialdj/urls.py
+++ social_django/redsocialdj/urls.py
@@ -12,6 +12,8 @@ video 5.2 -> se crea la vista el registro de usuarios
 video 5.5 -> hacemos la invocación en las rutas para las vistas de login y logout
 
 video 8.4 -> creamos la url para la creación del post 
+
+video 9.2 -> agregando la url para el perfil de los usuarios
 """
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,6 +23,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.feed, name='feed'),
     path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='social/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='social/logout.html'), name='logout'),
